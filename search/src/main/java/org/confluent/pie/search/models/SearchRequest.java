b3d099517e2531d961eb73e3ce9938f72689fa44
Pascal Vantrepote <42182336+pascalconfluent@users.noreply.github.com>
2024-11-22 14:27:07 -0500

    Adding metadata (#14)

--- search/src/main/java/org/confluent/pie/search/models/SearchRequest.java
+++ search/src/main/java/org/confluent/pie/search/models/SearchRequest.java
@@ -1,5 +1,7 @@
 package org.confluent.pie.search.models;
 
+import java.util.Map;
+
 /**
  * Search request object
  *
@@ -8,6 +10,29 @@ package org.confluent.pie.search.models;
  * @param numberOfCandidate number of candidate to retrieve from the index
  * @param limit             maximum number of results to return
  * @param minScore          minimum score to consider a result
+ * @param metadata          additional metadata to pass to the search
  */
-public record SearchRequest(String requestId, double[] embeddings, int numberOfCandidate, int limit, double minScore) {
+public record SearchRequest(String requestId, double[] embeddings, int numberOfCandidate, int limit, double minScore,
+                            Map<String, Object> metadata) {
+
+    public SearchRequest(String requestId, double[] embeddings, int numberOfCandidate, int limit, double minScore, Map<String, Object> metadata) {
+        this.requestId = requestId;
+        this.embeddings = embeddings;
+        this.numberOfCandidate = numberOfCandidate;
+        this.limit = limit;
+        this.minScore = minScore;
+        this.metadata = metadata;
+    }
+
+    public SearchRequest(String requestId, double[] embeddings, int numberOfCandidate, int limit, double minScore) {
+        this(requestId, embeddings, numberOfCandidate, limit, minScore, null);
+    }
+
+    public SearchRequest(String requestId, double[] embeddings, int numberOfCandidate, int limit) {
+        this(requestId, embeddings, numberOfCandidate, limit, 0.0, null);
+    }
+
+    public SearchRequest(String requestId, double[] embeddings) {
+        this(requestId, embeddings, 100, 10, 0.0, null);
+    }
 }
--- search/src/main/java/org/confluent/pie/search/models/SearchResults.java
+++ search/src/main/java/org/confluent/pie/search/models/SearchResults.java
@@ -5,14 +5,16 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.bson.Document;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * SearchResults represents the results of a search request
  *
  * @param requestId The request ID
  * @param results   The search results
+ * @param metadata  Additional metadata
  */
-public record SearchResults(String requestId, Document[] results) {
+public record SearchResults(String requestId, Document[] results, Map<String, Object> metadata) {
 
     /**
      * Create a SearchResults object from a pair of SearchRequest and a list of Products
@@ -23,7 +25,8 @@ public record SearchResults(String requestId, Document[] results) {
     public static SearchResults from(Pair<SearchRequest, List<Document>> products) {
         return new SearchResults(
                 products.getLeft().requestId(),
-                products.getRight().toArray(Document[]::new));
+                products.getRight().toArray(Document[]::new),
+                products.getLeft().metadata());
     }
 
 }
